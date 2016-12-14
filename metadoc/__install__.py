d8875ce9ce6de4dc12133624ea673a990978bcb7
psolbach <p@psolbach.com>
2016-12-14 14:25:37 +0100

    fix install tests

--- metadoc/__install__.py
+++ metadoc/__install__.py
@@ -3,20 +3,24 @@
 
 import nltk
 import time
-from metadoc.extract.pos import do_train
+from .extract.pos import do_train
 
-REQUIRED_CORPORA = [
-  'brown', # Required for FastNPExtractor
-  'punkt', # Required for WordTokenizer
-  'wordnet', # Required for lemmatization and Wordnet
-  'maxent_ne_chunker',
-  'stopwords',
-  'words' 
-]
+def install():
+  REQUIRED_CORPORA = [
+    'brown', # Required for FastNPExtractor
+    'punkt', # Required for WordTokenizer
+    'wordnet', # Required for lemmatization and Wordnet
+    'maxent_ne_chunker',
+    'stopwords',
+    'words' 
+  ]
 
-for each in REQUIRED_CORPORA:
-  print(('[+] Downloading corpus:  "{0}"'.format(each)))
-  nltk.download(each)
+  for each in REQUIRED_CORPORA:
+    print(('[+] Downloading corpus:  "{0}"'.format(each)))
+    nltk.download(each)
 
-print('[+] Training tagger now.')
-do_train()
+  print('[+] Training tagger now.')
+  do_train()
+
+if __name__ == "__main__":
+  install()
---
+++ tests/test_install.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import asyncio
+import asynctest
+import pytest
+
+from metadoc.__install__ import install
+
+class MetadocInstallTest(asynctest.TestCase):
+  def setUp(self):
+    return
+
+  @asynctest.ignore_loop
+  def test_install(self):
+    install()
