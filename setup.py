79b91a122955a30c36004ef9602b474b9996038f
Martin Borho <martin@borho.net>
2018-02-23 17:50:47 +0000

    Save nltk data in metadoc/extract/data and exclude nltk zip files from being installed.

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-import os
+import os.path
 import sys
 from subprocess import call
 from setuptools import setup, find_packages
 from setuptools.command.install import install as _install
 
+
+version_file = open('VERSION')
+version = version_file.read().strip()
 
 def _post_install():
         from metadoc.install import install_nltk_sets
@@ -14,12 +17,12 @@ def _post_install():
 class CustomInstall(_install):
     def run(self):
         call(["pip install -r requirements.txt --no-clean"], shell=True)
-        _install.run(self)
         self.execute(_post_install, (), msg="Installing nltk sets!")
+        _install.run(self)
 
 setup(
     name='metadoc',
-    version="0.1.0",
+    version=version,
     description="Post-truth era news article metadata service.",
     long_description="",
     classifiers=[ # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
