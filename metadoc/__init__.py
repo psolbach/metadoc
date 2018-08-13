438482ccdf5e487ec349196044f23a29bfe2cd63
Martin Borho <martin@borho.net>
2018-08-13 11:22:32 +0000

    New UA string

--- metadoc/__init__.py
+++ metadoc/__init__.py
@@ -180,8 +180,10 @@ class Metadoc(object):
         url = p.geturl()
 
         req = requests.get(url, headers={
-          'Accept-Encoding': 'identity, gzip, deflate, *',
-          'User-Agent': 'Facebot/1.0'
+          #'Accept-Encoding': 'identity, gzip, deflate, *',
+          #'User-Agent': 'Facebot/1.0'
+          #'User-Agent': 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)',
+          'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:61.0) Gecko/20100101 Firefox/61.0',
         })
 
         if req.status_code != 200:
