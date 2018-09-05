2338424728e41d92f538ea52877d545b0ae2e755
psolbach <p@psolbach.com>
2018-09-05 12:27:11 +0200

    remove _bdist_wheel setup

--- setup.py
+++ setup.py
@@ -32,15 +32,15 @@ class CustomInstall(_sdist):
         self.execute(_post_install, (), msg="Installing nltk sets!")
         _sdist.run(self)
 
-class BdistEggInstall(_bdist_wheel):
-   def run(self):
-        if sys.version_info[0] < 3 or if sys.version_info[1] < 6:
-            raise Exception("Must be using Python 3.6^")
+# class BdistEggInstall(_bdist_wheel):
+#    def run(self):
+#         if sys.version_info[0] < 3 or sys.version_info[1] < 6:
+#             raise Exception("Must be using Python 3.6^")
 
-        from wheel.bdist_wheel import bdist_wheel as _bdist_wheel
-        call(["pip install -r ./requirements.txt --no-clean"], shell=True)
-        self.execute(_post_install, (), msg="Installing nltk sets!")
-        _bdist_wheel.run(self)
+#         from wheel.bdist_wheel import bdist_wheel as _bdist_wheel
+#         call(["pip install -r ./requirements.txt --no-clean"], shell=True)
+#         self.execute(_post_install, (), msg="Installing nltk sets!")
+#         _bdist_wheel.run(self)
 
 setup(
     name='metadoc',
@@ -62,7 +62,7 @@ setup(
     author_email='p@psolbach.com',
     url='https://github.com/praise-internet/metadoc',
     license=metadata["license"],
-    cmdclass={'sdist': CustomInstall, 'develop': DevInstall, 'bdist_wheel': BdistEggInstall},
+    cmdclass={'sdist': CustomInstall, 'develop': DevInstall},
     packages=find_packages(exclude=['tests']),
     install_requires=requirements_txt.strip().split("\n"),
     include_package_data=True,
